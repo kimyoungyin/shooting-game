fda96464e92179cb4e784ea3a0f1f5fe0153637d
hhbb0081 <likesun2000@naver.com>
2022-09-28 23:08:25 +0900

    feat: 공격동작 구현 2차

--- clickGenerate/scenes/overlap.js
+++ clickGenerate/scenes/overlap.js
@@ -8,10 +8,7 @@ class overlap extends Phaser.Scene {
     this.load.image('button', 'img/button.png');
     this.load.image('tower', 'img/tower.png');
     this.load.image('platform', 'img/platform.png');
-    this.load.image('king', 'img/spriteSheet/Idle (1).png');
-    this.load.image('sword', 'img/sword.png');
-    this.load.image('dragon', 'img/dragon.png');
-    this.load.spritesheet('knight', 'img/knight.png', {
+    this.load.spritesheet('mummy', 'img/mummy.png', {
       frameWidth: 37, frameHeight: 45
     });
 
@@ -31,10 +28,7 @@ class overlap extends Phaser.Scene {
     this.enemy;
     this.attackEnemy;
     this.attacking = false; //공격 여부
-    this.isOverlap = false;
 
-
-    console.log(this.attacking);
     //버튼 누르면 햄스터 이동
     const sp = this.add.image(100, 520, 'button').setInteractive();
     sp.on('pointerdown', () => {
@@ -49,12 +43,11 @@ class overlap extends Phaser.Scene {
 
     this.knightAnimation = this.anims.create({
       key: 'attack',
-      //frames: this.anims.generateFrameNames('knight', { prefix: 'guard_start/frame', start: 0, end: 5 }),
-      frames: this.anims.generateFrameNumbers('knight'),
+      frames: this.anims.generateFrameNames('mummy'),
       frameRate: 16
     });
 
-    this.player = this.add.sprite(100, 400, 'knight').setScale(3);
+    this.player = this.add.sprite(100, 400, 'mummy').setScale(3);
 
     this.tweens.add({
       targets: player,
@@ -62,29 +55,30 @@ class overlap extends Phaser.Scene {
       duration: 8800,
       ease: 'Linear'
     });
-
-    //this.player.play({ key: 'attack', repeat: 10 });
-
     
   };
-
-  Play() {
-    this.player.play({ key: 'attack', repeat: 10 });
-  }
 
   update() {
     if (this.cursorOn === true && this.hamsters[this.hamsters.length - 1].x > 100) {
       this.hamsters.forEach(element => {
-        if (element.x > 100 ) {
-          element.x -= 5;
+        if (element.x > 100 || this.attacking === false) {
+          element.x -= 3;
         }
       });
     }
 
-    this.hamsters.forEach(ham => {
-      this.isOverlap = this.physics.add.overlap(this.player, ham, this.Play);
-      console.log(ham);
+    this.hamsters.forEach(element => {
+      
+      //미라 사정거리 내에 햄스터가 들어오면 attacking true
+      if (this.player.x + 90 > element.x) {
+        this.attacking = true;
+        //console.log(this.attacking);
+      }
+    
+      if (this.attacking === true) 
+      {
+        this.player.anims.play('attack', true);
+      }
     });
-    //겹쳤을 때
   };
 }
